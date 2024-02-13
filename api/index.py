ba21863d0bf1aa6b48d1b8e45dcd4ce957176b6f
r-marche <91833417+r-marche@users.noreply.github.com>
2024-02-13 15:19:22 +0100

    stringa

--- api/index.py
+++ api/index.py
@@ -1,7 +1,6 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
 class handler(BaseHTTPRequestHandler):
-    HTML_SOURCE = "<h1>Titolo</h1> <h2>Sottotitolo</h2>"
 
     def do_GET(self):
         self.send_response(200)
@@ -9,6 +8,22 @@ class handler(BaseHTTPRequestHandler):
         self.end_headers()
         
         #self.wfile.write(bytes(HTML_TEMPLATE, "utf8"))
-        
-        self.wfile.write("<h1>Titolo</h1> <h2>Sottotitolo</h2>".encode('utf-8'))
+        stringa = """
+<!DOCTYPE html>
+<html>
+<head>
+<title>Titolo</title>
+<style>
+body {{
+    font-family: Arial, sans-serif;
+}}
+</style>
+</head>
+<body>
+<h1>Titolo</h1>
+<h2>Sottotitolo</h2>
+</body>
+</html>
+"""
+        self.wfile.write(stringa.encode('utf-8'))
         return
