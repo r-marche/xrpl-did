ce3d0b49040eb30716be0937dd8f4c73422af7f5
r-marche <91833417+r-marche@users.noreply.github.com>
2024-02-13 14:44:43 +0100

    Update index.py

--- api/index.py
+++ api/index.py
@@ -6,5 +6,9 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
+        html_content = HTML_TEMPLATE.format(title="XRPL DID", subtitle="test-sottotitolo", background_color="#ffffff")
+        
+        self.wfile.write(bytes(html_content, "utf8"))
+        
         self.wfile.write('Hello, world!'.encode('utf-8'))
         return
