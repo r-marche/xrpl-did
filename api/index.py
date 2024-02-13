c3e9ac7b6e6c203cc565e767a311fb40fddcbdbe
r-marche <91833417+r-marche@users.noreply.github.com>
2024-02-13 15:13:15 +0100

    test complesso con encode

--- api/index.py
+++ api/index.py
@@ -1,7 +1,7 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
 class handler(BaseHTTPRequestHandler):
-    HTML_TEMPLATE = """
+    HTML_SOURCE = """
 <!DOCTYPE html>
 <html>
 <head>
@@ -21,5 +21,5 @@ class handler(BaseHTTPRequestHandler):
         
         #self.wfile.write(bytes(HTML_TEMPLATE, "utf8"))
         
-        self.wfile.write("<h1>Hello, world!</h1><h2> resto dell testo</h2>".encode('utf-8'))
+        self.wfile.write(HTML_SOURCE.encode('utf-8'))
         return
