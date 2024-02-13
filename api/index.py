177d46f4ae3c3bf67c2d96c6a2b04912ef7c5329
r-marche <91833417+r-marche@users.noreply.github.com>
2024-02-13 15:16:58 +0100

    boh

--- api/index.py
+++ api/index.py
@@ -1,7 +1,7 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
 class handler(BaseHTTPRequestHandler):
-    HTML_SOURCE = """ <h1>Titolo</h1> <h2>Sottotitolo</h2> """
+    HTML_SOURCE = "<h1>Titolo</h1> <h2>Sottotitolo</h2>"
 
     def do_GET(self):
         self.send_response(200)
